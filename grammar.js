7369c07ffdd2e8ccda81204ec5ee03dace8b42a0
mlysaght <michael.a.lysaght@gmail.com>
2023-05-04 11:11:04 +0100

    Add in grammar file for potential dialect

--- grammar.js
+++ grammar.js
@@ -1,12 +1,46 @@
 module.exports = grammar({
-  name: "gherkin",
+    name: 'gherkin',
+  
+    rules: {
+      feature_file: $ => repeat1($.feature),
+  
+      feature: $ => seq(
+        'Feature:',
+        $.any_string,
+        repeat1($.scenario)
+      ),
+  
+      scenario: $ => seq(
+        'Scenario:',
+        $.any_string,
+        $.given_statement,
+        seq(
+            'WHEN the following API calls are made:',
+            $.api_call_table,
+            'THEN alert'
+        )
+      ),
+  
+      given_statement: $ => seq(
+        'GIVEN',
+        'the',
+        $.service_name,
+        'service'
+      ),
+  
+      service_name: $ => /"[^"]*"/,
+  
+      api_call_table: $ => seq(
+        '|',
+        $.api_call_name,
+        '|',
+        repeat(seq('|', $.api_call_name, '|'))
+      ),
+  
+      api_call_name: $ => /[A-Za-z_0-9]+/,
+      
+      any_string: $ => /.*/,
 
-  rules: {
-    source: ($) => seq($.given, repeat1(seq($.when, optional(repeat1($.and)))), $.then),
-    given: ($) => seq("GIVEN", alias($.string, $.invariant)),
-    when: ($) => seq("WHEN", alias($.string, $.invariant)),
-    and: ($) => seq("AND", alias($.string, $.invariant)),
-    then: ($) => seq("THEN", alias($.string, $.invariant)),
-    string: $ => /.*/,
-  },
-});
+    }
+  });
+  
