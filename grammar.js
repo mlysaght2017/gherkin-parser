9f77549d6823d298563ac228b22a06556971ad2a
mlysaght <michael.a.lysaght@gmail.com>
2023-05-05 17:31:37 +0100

    Add in more generic parsing to align with gherkin standard

--- grammar.js
+++ grammar.js
@@ -1,46 +1,29 @@
+const awsServices = [
+    'S3',
+    'EC2',
+    'Lambda',
+    'DynamoDB',
+    'SNS',
+    'SQS',
+  ];
+  
+const awsServiceRegexList = awsServices.map(service => new RegExp(service));
+  
 module.exports = grammar({
     name: 'gherkin',
   
     rules: {
-      feature_file: $ => repeat1($.feature),
-  
-      feature: $ => seq(
-        'Feature:',
-        $.any_string,
-        repeat1($.scenario)
-      ),
-  
-      scenario: $ => seq(
-        'Scenario:',
-        $.any_string,
-        $.given_statement,
-        seq(
-            'WHEN the following API calls are made:',
-            $.api_call_table,
-            'THEN alert'
-        )
-      ),
+      feature_file: $ => $.given_statement,
   
       given_statement: $ => seq(
         'GIVEN',
-        'the',
-        $.service_name,
-        'service'
+        /.+?/,
+        seq(
+          prec.right(1, $.service_name),
+          optional(/.*/)
+        ),
       ),
-  
-      service_name: $ => /"[^"]*"/,
-  
-      api_call_table: $ => seq(
-        '|',
-        $.api_call_name,
-        '|',
-        repeat(seq('|', $.api_call_name, '|'))
-      ),
-  
-      api_call_name: $ => /[A-Za-z_0-9]+/,
-      
-      any_string: $ => /.*/,
 
-    }
-  });
-  
+      service_name: $ => choice(...awsServiceRegexList),
+  }
+});
