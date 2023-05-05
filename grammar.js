0cfb4fb46c479e97de47556638b9821af2890c9b
mlysaght <michael.a.lysaght@gmail.com>
2023-05-05 18:37:38 +0100

    Add in service description

--- grammar.js
+++ grammar.js
@@ -1,29 +1,61 @@
-const awsServices = [
-    'S3',
-    'EC2',
-    'Lambda',
-    'DynamoDB',
-    'SNS',
-    'SQS',
-  ];
-  
-const awsServiceRegexList = awsServices.map(service => new RegExp(service));
-  
+const api_calls = fs.read("api_calls.csv");
+
 module.exports = grammar({
-    name: 'gherkin',
-  
-    rules: {
-      feature_file: $ => $.given_statement,
-  
-      given_statement: $ => seq(
-        'GIVEN',
-        /.+?/,
-        seq(
-          prec.right(1, $.service_name),
-          optional(/.*/)
-        ),
+  name: "gherkin",
+
+  externals: [
+    $.external_identities,
+    $.error_sentinels
+  ],
+
+  rules: {
+    scene: ($) => $.given_statement,
+
+    // action: ($) ...
+
+    // expected: ($) ...
+
+    known_identities: [ // aws managed identies ]
+
+    s3_api_calls: $ => choice(
+      'CreateBucket',
+      'DeleteBucket',
+      'UpdateCORS',
+      // ....
+    ),
+    // ec2_api_calls
+    api_calls: $ => choice($.s3_api_calls, $.ec2_calls, ...)
+
+    and: ($) => "AND",
+    but: ($) => "BUT",
+    given: ($) => "GIVEN",
+
+    given_statement: ($) =>
+      seq(
+        seq($.given, $._service_description),
+        optional(seq($.given, choice($._service_description, _api_description, _identity_description))),
+        optional(seq($.and, $._service_description)),
+        optional(seq($.but, $._service_description))
       ),
 
-      service_name: $ => choice(...awsServiceRegexList),
-  }
+    // _api_description ...
+    // _identity_description
+
+    _service_description: ($) =>
+      repeat1(choice($.service_name, choice($.service_name, $.word))),
+
+    multiple_given_statements: ($) => seq($.given_statement),
+
+    word: ($) => /\w+/,
+    // service_name: $ => choice(...awsServiceRegexList),
+    service_name: ($) =>
+      choice(
+        choice("S3", "s3"),
+        choice("EC2", "ec2"),
+        "Lambda",
+        "DynamoDB",
+        "SNS",
+        "SQS"
+      ),
+  },
 });
