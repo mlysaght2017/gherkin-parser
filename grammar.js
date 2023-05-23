ac8397cf31ee404f69c884ba9b5de255e4882c9d
mlysaght <michael.a.lysaght@gmail.com>
2023-05-23 11:40:20 +0100

    Add in new modular rule for services

--- grammar.js
+++ grammar.js
@@ -1,78 +1,66 @@
-// module.exports = async () => {
-//     const { serviceNames, apiNames } = await readAWSServicesCSV('AwsServiceActions.csv');
-
-//     return grammar({
-//         name: 'gherkin',
-    
-//         rules: {
-//           document: ($) => repeat($._statement),
-    
-//           _statement: ($) => choice($.given_statement),
-    
-//           given_statement: ($) =>
-//             seq(
-//               seq($.given, choice(...serviceNames)),
-//               optional(seq($.given, choice(...serviceNames, ...apiNames))),
-//               optional(seq($.and, choice(...serviceNames))),
-//               optional(seq($.but, choice(...serviceNames)))
-//             ),
-    
-//           // Define other rules as needed
-//         },
-//       });
-//     };
 const services = require("./services");
-const apiCalls = require("./apiCalls");
-const grammar = grammar({
-  name: "gherkin",
 
-// //   externals: [
-// //     $.external_identities,
-// //     $.error_sentinels
-// //   ],
+module.exports = grammar({
+  name: 'gherkin',
 
   rules: {
-    scene: ($) => $.given_statement,
+    // Other grammar rules...
 
-    // action: ($) ...
-
-    // expected: ($) ...
-
-    // known_identities: [ // aws managed identies ]
-
-    // s3_api_calls: $ => choice(
-    //   'CreateBucket',
-    //   'DeleteBucket',
-    //   'UpdateCORS',
-    //   // ....
-    // ),
-    // // ec2_api_calls
-    // api_calls: $ => choice($.s3_api_calls, $.ec2_calls),
-
-    and: ($) => "AND",
-    but: ($) => "BUT",
-    given: ($) => "GIVEN",
-
-    given_statement: ($) =>
-      seq(
-        seq($.given, $._service_description),
-        optional(seq($.given, choice($._service_description, _api_description))),
-        optional(seq($.and, $._service_description)),
-        optional(seq($.but, $._service_description))
-      ),
-
-    _service_description: ($) =>
-      repeat1(choice($.service_name, choice($.service_name, $.word))),
-
-    multiple_given_statements: ($) => seq($.given_statement),
-
-    word: ($) => /\w+/,
-
+    awsServiceName: $ => seq(
+      choice(...services.awsServiceName)
+    ),
   },
 });
 
-for (const serviceName in services) {
-  grammar.rules[serviceName] = services[serviceName];
-}
 
-module.exports = grammar;
+
+// const services = require("./services");
+// // const apiCalls = require("./apiCalls");
+// const grammar = grammar({
+//   name: "gherkin",
+
+//   rules: {
+//     scene: ($) => $.given_statement,
+
+//     // action: ($) ...
+
+//     // expected: ($) ...
+
+//     // known_identities: [ // aws managed identies ]
+
+//     // s3_api_calls: $ => choice(
+//     //   'CreateBucket',
+//     //   'DeleteBucket',
+//     //   'UpdateCORS',
+//     //   // ....
+//     // ),
+//     // // ec2_api_calls
+//     // api_calls: $ => choice($.s3_api_calls, $.ec2_calls),
+
+//     and: ($) => "AND",
+//     but: ($) => "BUT",
+//     given: ($) => "GIVEN",
+
+//     given_statement: ($) =>
+//       seq(
+//         seq($.given, $._service_description),
+//         optional(seq($.given, choice($._service_description, _api_description))),
+//         optional(seq($.and, $._service_description)),
+//         optional(seq($.but, $._service_description))
+//       ),
+
+//     _service_description: ($) =>
+//       repeat1(choice($.service_name, choice($.service_name, $.word))),
+
+//     multiple_given_statements: ($) => seq($.given_statement),
+
+//     word: ($) => /\w+/,
+
+//   },
+// });
+
+// for (const serviceName in services) {
+//   grammar.rules[serviceName] = services[serviceName];
+// }
+
+// module.exports = grammar;
--- services.js
+++ services.js
@@ -1,4 +1,16 @@
-module.exports = {
-    's3' : ($) => seq(...),
-    'ec2': ($) => ...
-};
+const services = {
+    awsServiceName: [
+      createAwsServiceRule("s3"),
+      createAwsServiceRule("ec2"),
+      createAwsServiceRule("sns"),
+      // Add more AWS service rules here
+    ],
+  };
+  
+  function createAwsServiceRule(serviceName) {
+    // Modify this function to define the rule for the AWS service name
+    return new RegExp(serviceName, "i");
+  }
+  
+  module.exports = services;
+  
