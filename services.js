650f693b365d8179c4f508aff1e3a32748838cfb
mlysaght <michael.a.lysaght@gmail.com>
2023-06-02 18:54:46 +0100

    Add in api names

--- services.js
+++ services.js
@@ -9,11 +9,17 @@ function createServicesFromCSV(filePath) {
 
   for (const row of rows) {
     const columns = row.split(',');
-    const action = columns[1]?.trim(); // Assuming 'Action' is in the second column
-    const serviceName = action?.split(':')[0];
+    const serviceAction = columns[1]?.trim(); // Assuming 'Action' is in the second column
+    const serviceName = serviceAction?.split(':')[0];
+    const action = serviceAction?.split(':')[1]?.trim();
+    
     if (serviceName && !awsServices[serviceName]) {
       awsServices[serviceName] = createAwsServiceRule(serviceName);
     }
+    
+    // if (action && !awsServices[action]) {
+    //   awsServices[action] = createActionRule(action);
+    // }
   }
 
   return awsServices;
@@ -28,11 +34,24 @@ function createAwsServiceRule(serviceName) {
       return char;
     }
   }).join("");
+  
   return new RegExp(serviceName);
+}
+
+function createActionRule(action) {
+  action = action.trim().replace(/^\"/, "").replace(/\"$/, "");
+  action = action.split("").map(char => {
+    if (char.match(/[a-zA-Z]/)) {
+      return `[${char.toLowerCase()}${char.toUpperCase()}]`;
+    } else {
+      return char;
+    }
+  }).join("");
+  
+  return new RegExp(action);
 }
 
 const csvFilePath = './AwsServiceActions.csv';
 const services = createServicesFromCSV(csvFilePath);
 
 module.exports = services;
-  
